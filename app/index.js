c41b9a3928e8b8f141f0b92a0d6cc98500f443ff
Douglas Alves <douglas.expalves@gmail.com>
2017-10-20 22:40:55 -0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,9 +1,22 @@
 // @flow
+import hapi from 'hapi'
+import config from './config'  
 
-import hapi from 'hapi';
+let Hapi = new hapi.Server()
 
-function foo(x: ?string): string | void {
-  if (x) {
-    return 'default string';   // simple example
+Hapi.connection({ port: config.port, host: config.hostname })
+
+Hapi.start((err: Error) => {
+  if (err) {
+    console.error(err.message)
+    process.exit(1)
   }
-}
+
+  console.log('%s rodando na %s', Hapi.info.host, Hapi.info.port)
+})
+
+// function foo(x: ?string): string | void {
+//   if (x) {
+//     return 'default string' // simple example
+//   }
+// }
