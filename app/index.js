9bc817d2dae3be512416e3ed32eba4af6adf3c80
Douglas Alves <douglas.expalves@gmail.com>
2017-12-04 23:17:38 -0200

    removed hapi-router usage from the project

--- app/index.js
+++ app/index.js
@@ -3,30 +3,15 @@ import info from './config'
 import Session from './entities/sessions/route'
 
 let hapi = new Server()
-// let Sessao
 
 hapi.connection({ port: info.port, host: info.hostname })
 
 hapi.route([...Session])
 
-hapi.register(
-  {
-    register: require('hapi-router'),
-    options: {
-      routes: ['entities/sessions/route.js']
-    }
-  },
-  err => {
-    if (err) {
-      console.error(err.message)
-      process.exit(1)
-    }
-    hapi.start(err => {
-      if (err) {
-        console.error(err.message)
-        process.exit(1)
-      }
-      console.log('%s rodando na %s', hapi.info.host, hapi.info.port)
-    })
+hapi.start(err => {
+  if (err) {
+    console.error(err.message)
+    process.exit(1)
   }
-)
+  console.log('%s rodando na %s', hapi.info.host, hapi.info.port)
+})
